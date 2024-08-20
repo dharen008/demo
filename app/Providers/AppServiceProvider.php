669d6edf57d90d55402cc0e923c6bc1971eaed49
dharen008 <109984612+dharen008@users.noreply.github.com>
2024-08-21 01:39:58 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,7 @@ use Filament\Tables\Actions\DeleteBulkAction;
 use Filament\Tables\Actions\DetachAction;
 use Filament\Tables\Actions\ForceDeleteBulkAction;
 use Filament\Tables\Actions\RestoreBulkAction;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,50 +28,66 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        DeleteAction::configureUsing( function ( DeleteAction $detachAction ): void {
-            $detachAction
+        DeleteAction::configureUsing( function ( DeleteAction $deleteAction ): void {
+            $deleteAction
+                ->successNotificationTitle( 'Custom Title' )
+                ->successNotification( fn () => dump( 'Custom Notification' ) )
                 ->modalHeading( 'Custom heading' )
                 ->modalDescription( 'Custom description' );
         } );
 
         RestoreAction::configureUsing( function ( RestoreAction $restoreAction ): void {
             $restoreAction
+                ->successNotificationTitle( 'Custom Title' )
+                ->successNotification( fn () => dump( 'Custom Notification' ) )
                 ->modalHeading( 'Custom heading' )
                 ->modalDescription( 'Custom description' );
         } );
 
         ForceDeleteAction::configureUsing( function ( ForceDeleteAction $forceDeleteAction ): void {
             $forceDeleteAction
+                ->successNotificationTitle( 'Custom Title' )
+                ->successNotification( fn () => dump( 'Custom Notification' ) )
                 ->modalHeading( 'Custom heading' )
                 ->modalDescription( 'Custom description' );
         } );
 
         AttachAction::configureUsing( function ( AttachAction $attachAction ): void {
             $attachAction
+                ->successNotificationTitle( 'Custom Title' )
+                ->successNotification( fn () => dump( 'Custom Notification' ) )
                 ->modalHeading( 'Custom heading' )
                 ->modalDescription( 'Custom description' );
         } );
 
         DetachAction::configureUsing( function ( DetachAction $detachAction ): void {
             $detachAction
+                ->successNotificationTitle( 'Custom Title' )
+                ->successNotification( fn () => dump( 'Custom Notification' ) )
                 ->modalHeading( 'Custom heading' )
                 ->modalDescription( 'Custom description' );
         } );
 
         DeleteBulkAction::configureUsing( function ( DeleteBulkAction $deleteBulkAction ): void {
             $deleteBulkAction
+                ->successNotificationTitle( 'Custom Title' )
+                ->successNotification( fn () => dump( 'Custom Notification' ) )
                 ->modalHeading( 'Custom heading' )
                 ->modalDescription( 'Custom description' );
         } );
 
         RestoreBulkAction::configureUsing( function ( RestoreBulkAction $restoreBulkAction ): void {
             $restoreBulkAction
+                ->successNotificationTitle( 'Custom Title' )
+                ->successNotification( fn () => dump( 'Custom Notification' ) )
                 ->modalHeading( 'Custom heading' )
                 ->modalDescription( 'Custom description' );
         } );
 
         ForceDeleteBulkAction::configureUsing( function ( ForceDeleteBulkAction $forceDeleteBulkAction ): void {
             $forceDeleteBulkAction
+                ->successNotificationTitle( 'Custom Title' )
+                ->successNotification( fn () => dump( 'Custom Notification' ) )
                 ->modalHeading( 'Custom heading' )
                 ->modalDescription( 'Custom description' );
         } );
